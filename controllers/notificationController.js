5235cbf155e081bef0bb5e3c503298f3770e9741
harleen <h251@mylangara.ca>
2024-04-02 12:19:43 -0700

    P2S-238: added new endpoint to count unread notifications

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -196,4 +196,14 @@ const markNotificationAsRead = async (req, res) => {
 };
 
 
-module.exports = { getNotification, getActiveNotificationList, markNotificationAsRead  }
+const getUnreadNotificationCount = async (req, res) => {
+    try {
+        const unreadCount = await Notification.countDocuments({ read: false });
+        res.status(200).json({ unreadCount });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+module.exports = { getNotification, getActiveNotificationList, markNotificationAsRead, getUnreadNotificationCount  }
